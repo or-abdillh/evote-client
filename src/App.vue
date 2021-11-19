d478f612c37bb589cf15b559127380bf3155691d
or-abdillh <hans.abdillh05@gmail.com>
2021-11-19 23:48:42 +0800

    Install pinia for state mangement

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1 class="text-center">Evote App</h1>
+  <h1 class="text-center">Evote App : {{ counter.count }}</h1>
 </template>
+
+<script setup>
+
+  import { useCounter } from './stores/counter'
+
+
+  const counter = useCounter()
+  
+
+</script>
