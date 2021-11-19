95d77c3ae1d83fcfb070714bd5d81008fec38f7f
or-abdillh <hans.abdillh05@gmail.com>
2021-11-20 00:23:31 +0800

    Create file app.css

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router/'
 import App from './App.vue'
-
 import './style/index.css'
+import './style/app.css'
 
 createApp(App)
 .use(createPinia())
