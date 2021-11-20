e894eea5a361e01ee553debf7ea5a14bd01a812b
or-abdillh <hans.abdillh05@gmail.com>
2021-11-20 23:04:52 +0800

    Create Header component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
-  <h1 class="text-center">Evote App : {{ counter.count }}</h1>
+   <Header></Header>
   <router-view></router-view>
 </template>
 
 <script setup>
-
-  import { useCounter } from './stores/counter'
-
-
-  const counter = useCounter()
-  
-
+   import Header from './components/Header.vue'
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+   <header>
+      <div class="header-wrapper">
+         <span>
+            <img src="/logo.png" width="55" />
+         </span>
+         <span>
+            <h1 class="text-gray-50 font-semibold">EVOTING HIMA TI</h1>
+         </span>
+      </div>
+   </header>
+</template>
+
+<style scoped>
+   
+   .header-wrapper {
+      @apply w-full px-3 py-2 flex items-center gap-2;
+      background: var(--secondary);
+   }
+   
+</style>
