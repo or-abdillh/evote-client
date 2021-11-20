ee0659827285ccd4a84b7b7f952e7cf979268fa5
or-abdillh <hans.abdillh05@gmail.com>
2021-11-20 23:29:08 +0800

    Create SectionCard component and slot

---
+++ src/components/Home/SectionCard.vue
@@ -0,0 +1,24 @@
+<template>
+   <section>
+      <div :class="borderColor" class="section-card-wrapper">
+         <slot name="card-content"></slot>
+      </div>
+   </section>
+</template>
+
+<style scoped>
+   .section-card-wrapper {
+      @apply text-center bg-gray-50 px-4 py-3 rounded-lg shadow border-l-8;
+   }
+</style>
+
+<script setup>
+   
+   //Define props
+   const props = defineProps({
+      borderColor: {
+         type: String,
+         default: 'border-green-500'
+      }
+   })
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,31 @@
 <template>
-  This is Home
+   <section>
+      <div class="home-wrapper">
+         <!-- Welcome -->
+         <SectionCard borderColor="border-blue-500">
+            <template v-slot:card-content>
+               <strong>Selamat Datang</strong>
+               <p>Pemilihan Ketua Umum dan Wakil Ketua Umum HIMA TI Polihasnur Tahun 2021/2022</p>
+            </template>
+         </SectionCard>
+      </div>
+   </section>
 </template>
+
+<script setup>
+   import SectionCard from '../components/Home/SectionCard.vue'
+</script>
+
+<style scoped>
+   .home-wrapper {
+      @apply px-3 py-2;
+   }
+   
+   .home-wrapper p {
+      @apply text-xs;
+   }
+   
+   .home-wrapper strong {
+      @apply text-sm;
+   }
+</style>
