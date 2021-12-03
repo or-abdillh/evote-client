be7c354ca83cd0bb0f3a7ecbb1422311cf5ca545
or-abdillh <hans.abdillh05@gmail.com>
2021-12-03 22:44:18 +0800

    Create handler for make a guard navigation

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import http from '../API/http.js'
 import Home from '../views/Home.vue'
 import Voting from '../views/Voting.vue'
 import Login from '../views/Login.vue'
@@ -21,5 +22,14 @@ const routes = [
 	}
 ]
 const router = createRouter({ history: createWebHistory(process.env.BASE_URL), routes })
+
+//Navigation Guard
+router.beforeEach((to, next) => {
+	http.get('auth', (data, response = true) => {
+		//Auth token success
+		if (response && to.name !== 'login') next()
+		else router.push({ name: 'login' }) //Fail 
+	})
+})
 
 export default router
